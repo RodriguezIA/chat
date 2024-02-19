a294681cff2c5bccc17cb3c6f5a133a40daf1845
RodriguezIA <edgaralanra@gmail.com>
2024-02-18 19:13:48 -0600

    actualizacion de nombre para el cliente

--- server/src/db/account.ts
+++ server/src/db/account.ts
@@ -1,9 +1,9 @@
-import { prismaClient } from "../../prisma/client";
+import { client } from "../../prisma/client";
 import { CreateAccountSchema } from '../models/account'
 import { z } from "zod";
 
 export const create = async(data: z.infer<typeof CreateAccountSchema>) => {
-    const query = await prismaClient.account.create({
+    const query = await client.account.create({
         data: {
             email: data.email,
             password: data.password
